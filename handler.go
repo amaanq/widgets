60baebfcfa9113ae50f233e75c8208339e0a815d
Amaan <amaanq12@gmail.com>
2022-02-20 14:31:05 -0500

    handler bug fix

--- handler.go
+++ handler.go
@@ -6,33 +6,58 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-func (p *Paginator) AddButtonHandler(ses *discordgo.Session, msg *discordgo.MessageSend, button discordgo.Button, callback func(s *discordgo.Session, i *discordgo.InteractionCreate)) error {
-	if ses == nil || msg == nil {
+func (p *Paginator) AddButtonHandler(ses *discordgo.Session /*msg *discordgo.MessageSend,*/, button discordgo.Button, callback func(s *discordgo.Session, i *discordgo.InteractionCreate)) error {
+	if ses == nil /* || msg == nil */ {
 		return errors.New("session and message cannot be nil")
 	}
 	f := func() error {
 		var s, last int = 0, 0
-		for idx, component := range msg.Components {
-			actionRow := component.(discordgo.ActionsRow)
-			s += len(actionRow.Components)
-			last = idx
-		}
-		if s == 25 {
-			return errors.New("max number of buttons allowed")
-		}
-		if s == 0 {
-			actionRow := discordgo.ActionsRow{
-				Components: []discordgo.MessageComponent{button},
+		// for idx, component := range msg.Components {
+		// 	actionRow := component.(discordgo.ActionsRow)
+		// 	s += len(actionRow.Components)
+		// 	last = idx
+		// }
+		// if s == 25 {
+		// 	return errors.New("max number of buttons allowed")
+		// }
+		// if s == 0 {
+		// 	actionRow := discordgo.ActionsRow{
+		// 		Components: []discordgo.MessageComponent{button},
+		// 	}
+		// 	msg.Components = []discordgo.MessageComponent{actionRow}
+		// } else if s%5 == 0 {
+		// 	actionRow := msg.Components[last].(discordgo.ActionsRow)
+		// 	actionRow.Components = append(actionRow.Components, button)
+		// 	msg.Components[last] = actionRow
+		// } else {
+		// 	actionRow := msg.Components[last].(discordgo.ActionsRow)
+		// 	actionRow.Components = append(actionRow.Components, button)
+		// 	msg.Components = append(msg.Components[:last], actionRow)
+		// }
+		p.Lock()
+		for _, page := range p.Pages {
+			for idx, component := range page.Components {
+				actionRow := component.(discordgo.ActionsRow)
+				s += len(actionRow.Components)
+				last = idx
 			}
-			msg.Components = []discordgo.MessageComponent{actionRow}
-		} else if s%5 == 0 {
-			actionRow := msg.Components[last].(discordgo.ActionsRow)
-			actionRow.Components = append(actionRow.Components, button)
-			msg.Components[last] = actionRow
-		} else {
-			actionRow := msg.Components[last].(discordgo.ActionsRow)
-			actionRow.Components = append(actionRow.Components, button)
-			msg.Components = append(msg.Components[:last], actionRow)
+			if s == 25 {
+				return errors.New("max number of buttons allowed")
+			}
+			if s == 0 {
+				actionRow := discordgo.ActionsRow{
+					Components: []discordgo.MessageComponent{button},
+				}
+				page.Components = []discordgo.MessageComponent{actionRow}
+			} else if s%5 == 0 {
+				actionRow := page.Components[last].(discordgo.ActionsRow)
+				actionRow.Components = append(actionRow.Components, button)
+				page.Components[last] = actionRow
+			} else {
+				actionRow := page.Components[last].(discordgo.ActionsRow)
+				actionRow.Components = append(actionRow.Components, button)
+				page.Components = append(page.Components[:last], actionRow)
+			}
 		}
 		ses.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			if i.MessageComponentData().CustomID == button.CustomID {
